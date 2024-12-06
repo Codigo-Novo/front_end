9ea3690f11178e598d6ba8c1c3575d2c805c6a03
MatheusOliveiraT <15matheusdeoliveira@gmail.com>
2024-12-06 15:04:24 -0300

    Map Markers

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,19 +1,46 @@
 import { Component } from '@angular/core';
-import { GoogleMapsModule } from "@angular/google-maps";
-import { HeaderComponent } from "../header/header.component";
-import { FooterComponent } from "../footer/footer.component";
+import { GoogleMapsModule } from '@angular/google-maps';
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-map',
   standalone: true,
   imports: [GoogleMapsModule, HeaderComponent, FooterComponent],
   templateUrl: './map.component.html',
-  styleUrl: './map.component.css'
+  styleUrls: ['./map.component.css']
 })
-export class MapComponent {
+export class MapComponent implements OnInit {
   options: google.maps.MapOptions = {
-    mapId: "dba8017d56ad3011",
     center: { lat: -24.043779373168945, lng: -52.38108825683594 },
     zoom: 15,
   };
+  
+  marcadores: { position: google.maps.LatLngLiteral; title: string }[] = [
+    { position: { lat: -24.043779, lng: -52.381088 }, title: 'Marcador 1' },
+    { position: { lat: -24.045, lng: -52.382 }, title: 'Marcador 2' },
+  ];
+
+  ngOnInit() {
+    var map = new google.maps.Map(document.getElementById('map-canvas')!, this.options);
+    this.adicionarMarcadores(map);
+  }
+
+  adicionarMarcadores(map: google.maps.Map) {
+    this.marcadores.forEach((marcador) => {
+      console.log("Adicionando marcador:", marcador);
+      const gMarker = new google.maps.Marker({
+        position: marcador.position,
+        map: map,
+        title: marcador.title,
+      });
+      const infoWindow = new google.maps.InfoWindow({
+        content: marcador.title,
+      });
+      gMarker.addListener('click', () => {
+        infoWindow.open(map, gMarker);
+      });
+    });
+  }
 }
