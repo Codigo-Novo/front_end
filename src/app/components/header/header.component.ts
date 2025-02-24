b7a389b59cb9c0e0d1433870899e30a4d40887ef
MatheusOliveiraT <15matheusdeoliveira@gmail.com>
2025-02-24 16:01:23 -0300

    Icon update + HTML span

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,16 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { ApiService } from '../../api.service';
 import { NgIf } from '@angular/common';
+import { ModalComponent } from '../modal/modal.component';
 
 @Component({
     selector: 'app-header',
-    imports: [RouterLink, NgIf],
+    imports: [RouterLink, NgIf, ModalComponent],
     standalone: true,
     templateUrl: './header.component.html',
     styleUrl: './header.component.css'
 })
 export class HeaderComponent implements OnInit {
 
+  showModal: boolean = false;
   logged_in: boolean = false;
 
   constructor(private api: ApiService, private router: Router) { }
@@ -24,5 +26,13 @@ export class HeaderComponent implements OnInit {
   async logout() {
     await this.api.logout();
     this.router.navigate(['/']);
+  }
+
+  openModal() {
+    this.showModal = true;
+  }
+
+  closeModal() {
+    this.showModal = false;
   }
 }
